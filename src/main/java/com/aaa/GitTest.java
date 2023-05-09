0d286e95e337cacef1eb5b0b56d02fd1c2956f8d
shj <shj@qq.com>
2023-05-09 14:12:09 +0800

    tow commit

--- src/main/java/com/aaa/GitTest.java
+++ src/main/java/com/aaa/GitTest.java
@@ -3,5 +3,6 @@ package com.aaa;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("11111111111");
+        System.out.println("22222222222");
     }
 }
